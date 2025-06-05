af00b86d5387a51fdb0702129591ce00facd3a45
idacarlin95 <carlin95@hotmail.se>
2025-06-05 15:45:55 +0200

    Removed inline styling and changed font styling to use variables

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,14 +3,7 @@ import Link from "next/link"
 const Header = () => {
     return (
         <header className="fixed top-0 left-0 w-full z-50 bg-transparent py-4" >
-            <nav className="flex justify-center items-center gap-10 sm:gap-10 md:gap-24 text-sm sm:text-base md:text-lg px-[20px] h-[44px]"
-                style={{
-                    color: "var(--color-elwhite)",
-                    fontFamily: "var(--font-elmain), sans-serif",
-                    fontWeight: '500',
-                    textShadow:
-                        '-1px 2px 3px rgba(117, 32, 29, 0.9), 1px 2px 4px rgba(117, 32, 29, 0.9)'
-                }}>
+            <nav className="flex justify-center items-center gap-10 md:gap-24 text-elbodys sm:text-elbodym md:text-elbodyl px-[20px] h-[44px] text-elwhite font-elmain font-medium text-shadow-soft">
                 <Link href="/">Home</Link>
                 <Link href="/gameplay">Gameplay</Link>
                 <Link href="/aboutus">About Us</Link>
@@ -20,3 +13,4 @@ const Header = () => {
 }
 
 export default Header
+
