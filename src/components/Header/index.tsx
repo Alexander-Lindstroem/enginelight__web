2c59d3f4bcfd38763f6d32c1f7e5b89e528037c7
idacarlin95 <carlin95@hotmail.se>
2025-06-05 13:03:30 +0200

    Added the variables

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,8 +5,8 @@ const Header = () => {
         <header className="fixed top-0 left-0 w-full z-50 bg-transparent py-4" >
             <nav className="flex justify-center items-center gap-10 sm:gap-10 md:gap-24 text-sm sm:text-base md:text-lg px-[20px] h-[44px]"
                 style={{
-                    color: "#CBCBCB",
-                    fontFamily: "'Orbitron', sans-serif",
+                    color: "var(--color-elwhite)",
+                    fontFamily: "var(--font-elmain), sans-serif",
                     fontWeight: '500',
                     textShadow:
                         '-1px 2px 3px rgba(117, 32, 29, 0.9), 1px 2px 4px rgba(117, 32, 29, 0.9)'
