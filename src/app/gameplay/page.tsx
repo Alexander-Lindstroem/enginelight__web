741057ce655126f8ea48a85b478eeb0449266e96
VaishaliGovindaraj <vaishali.govindaraj28@gmail.com>
2025-06-10 00:04:36 +0200

    adding snapshot_1 changes

---
+++ src/app/gameplay/page.tsx
@@ -0,0 +1,11 @@
+import Gameplay_world from "@/components/GamePlay_Snapshot1"
+
+const Gameplay = () => {
+    return (
+        <>
+        <Gameplay_world />
+        </>
+    )
+}
+
+export default Gameplay
