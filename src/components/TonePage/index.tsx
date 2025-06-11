ff52920caa37b78eada1e49f82e554671cbfd3d7
kavitha0691 <kavitha0691@gmail.com>
2025-06-11 21:17:30 +0200

    fixing font moving on different screen sizes

--- src/components/TonePage/index.tsx
+++ src/components/TonePage/index.tsx
@@ -7,18 +7,18 @@ export default function TonePage() {
     <div className="relative w-full h-screen overflow-hidden">
       <Image
         src="/gameplay2.jpg"
-        alt= "Tone Background"
+        alt="Tone Background"
         fill
         className="object-cover object-center"
         quality={100}
         priority
       />
       <div className="absolute inset-0 bg-opacity-60 z-10" />
-      <div className="absolute z-20 top-[32%] font-elmain">
-        <div className="ml-4 sm:ml-10 md:ml-32 lg:ml-[44rem] max-w-xl text-left text-[var(--color-elwhite)] space-y-5 tracking-wider px-4 sm:px-0">
-          <h2 className="text-elh1 font-bold">Tone</h2>
-          <p className="text-elbodym leading-8 mb-0"> The visuals are dark, gritty and moody but the low-poly stylization keeps it lighthearted. </p>
-          <p className="text-elbodym leading-8"> The dialogue, mechanics and events of the game contrasts this darkness with a lot of goofiness and comedy.</p>
+      <div className="absolute inset-0 z-20 flex items-center justify-start font-elmain px-6 sm:px-10 md:px-20 lg:px-0 lg:pl-[46rem]">
+        <div className="max-w-xl text-left text-[var(--color-elwhite)] space-y-5 tracking-wider">
+          <h2 className="text-elh1 lg:text-elh1 md:text-elh1 sm:text-elh3 font-bold">Tone</h2>
+          <p className="text-elbodyl lg:text-elbodym md:text-elbody1 sm:text-elbodym leading-8 mb-0"> The visuals are dark, gritty and moody but the low-poly stylization keeps it lighthearted. </p>
+          <p className="text-elbodyl lg:text-elbodym md:text-elbodyl sm:text-elbodym leading-8"> The dialogue, mechanics and events of the game contrasts this darkness with a lot of goofiness and comedy.</p>
         </div>
       </div>
     </div>
