872012522648f00ee4bdd7054437e14ee9336ec1
VaishaliGovindaraj <vaishali.govindaraj28@gmail.com>
2025-06-11 23:13:23 +0200

    updating the review comment changes

--- src/components/GamePlay_Snapshot1/index.tsx
+++ src/components/GamePlay_Snapshot1/index.tsx
@@ -1,16 +1,22 @@
 const Gameplay_world = () => {
     return (
 
-        <div className="h-screen w-screen bg-[url('/GamePlay_snapshot1.jpg')] bg-cover bg-no-repeat flex items-center justify-center">
-            <div className="absolute top-60 left-10 bg-black/15 max-w-50 p-6 rounded-lg  xs:left-30  xs:max-w-[300px] md:max-w-[500px] md:left-60 md:top-80 lg:max-w-[700px]">
-                <p className="font-elmain text-elwhite  text-3xl mb-4 font-bold ">The world</p>
-                <p className="font-elmain text-elwhite mb-4">
+
+        <div className="min-h-screen w-screen bg-[url('/GamePlay_snapshot1.jpg')] bg-cover  bg-[rgba(0,0,0,0.2)] bg-blend-multiply bg-no-repeat flex items-start justify-start p-10">
+            <div className="bg-black/15 p-6 mt-65 ml-10 rounded-lg z-10 max-w-full xs:max-w-[300px]  md:mt-60 md:ml-15 md:max-w-[500px] lg:max-w-[600px] lg:mt-60 lg:ml-15 xl:max-w-[600px]">
+                <h2 className="font-elmain text-elwhite text-3xl mb-4 font-bold">
+                    The world
+                </h2>
+
+                <p className="font-elmain text-elwhite text-base/8">
                     Discover the Open World of Nosviken, infested with old mechanical war robots, or "Mechs", as the locals call them.
                 </p>
-                <p className="font-elmain text-elwhite mb-4">
+
+                <p className="font-elmain text-elwhite text-base/8">
                     Explore the world and help the local prepper & gas station owner liberate Nosviken from the invasion. Use any means necessary to take them down.
                 </p>
-                <p className="font-elmain text-elwhite">
+
+                <p className="font-elmain text-elwhite text-base/8">
                     Pimp your ride with cosmetic dashboard collectibles.
                 </p>
             </div>
