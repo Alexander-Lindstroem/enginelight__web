1c51df0db91831e0c49565dd6ff03133a84fb361
VaishaliGovindaraj <vaishali.govindaraj28@gmail.com>
2025-06-11 12:25:08 +0200

    Adding Video component in GamePlay page

--- src/app/gameplay/page.tsx
+++ src/app/gameplay/page.tsx
@@ -1,11 +1,9 @@
-import Dummy from "@/components/dummy";
 import VideoContainer from "@/components/videocontainer";
 
 const Gameplay = () => {
     return (
         <>
         <VideoContainer video_link="/GP3_Team2_video.mp4" />
-        {/* <Dummy /> */}
         </>
     )
 }
--- src/components/videocontainer/index.tsx
+++ src/components/videocontainer/index.tsx
@@ -1,18 +1,23 @@
 'use client'
 
-import {  useState } from 'react'
-import { Volume2, VolumeX } from 'lucide-react' 
+import { useState, useEffect } from 'react'
+import { Volume2, VolumeX } from 'lucide-react'
+import { toggleMute,updateIconSize} from '@/utils/functions'
 
+const VideoContainer = ({ video_link }: { video_link: string }) => {
+  const [isMuted, setIsMuted] = useState<boolean>(true)
+  const [iconSize, setIconSize] = useState<number>(32)
 
-const VideoContainer = ({video_link}:  { video_link: string }) => {
-      const [isMuted, setIsMuted] = useState(true)
+  useEffect(() => {
+   
+    const handleResize = () => updateIconSize(setIconSize)
+    handleResize()
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
 
-const toggleMute = () => {
-    setIsMuted(prev => !prev)
-  }
-
-    return(
-       <div className="relative w-[100%] h-screen mx-auto">
+  return (
+    <div className="relative w-[100%] h-screen">
       <video
         src={video_link}
         className="absolute top-0 left-0 w-full h-full object-cover"
@@ -22,11 +27,11 @@ const toggleMute = () => {
         playsInline
       />
       <button
-        onClick={toggleMute}
-        className="absolute top-4 right-4 bg-black/50 p-2 rounded-full text-white" >
-        {isMuted ? <VolumeX /> : <Volume2 />}
+        onClick={() => toggleMute(setIsMuted)}
+        className="absolute top-20 right-20 bg-white/50 p-2 rounded-xl text-black text-3xl sm:text-7xl" >
+        {isMuted ? <VolumeX size={iconSize} /> : <Volume2 size={iconSize} />}
       </button>
     </div>
-    )
+  )
 }
 export default VideoContainer;
