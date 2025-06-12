b77d3e71ef2fda47bc1ad47ad3ebf9b87025a432
alelin <alexanderbdjgw@gmail.com>
2025-06-12 15:28:58 +0200

    fixed mobile responsiveness for home page and adjusted buttons to be more uniform

--- src/components/DescriptionContent/index.tsx
+++ src/components/DescriptionContent/index.tsx
@@ -8,9 +8,9 @@ const DescriptionContent = () => {
         <div className="h-full flex flex-col gap-5 font-elmain justify-center">
           <div className="grow-1"></div>
           <div className="flex flex-col gap-2 border-white border md:w-1/2 w-full p-2">
-            <h2 className="text-elh2 font-bold">"A Swedish Mad Max"</h2>
+            <h2 className="md:text-elh2 text-elh4 font-bold">"A Swedish Mad Max"</h2>
             <h2 className="font-medium">FPS</h2>
-            <div>
+            <div className="text-elbodys">
               <p>Genres: Combat & Survival</p>
               <p>Art style: Low-Poly Stylization</p>
               <p>Setting: Desolate Northern Sweden</p>
@@ -19,23 +19,25 @@ const DescriptionContent = () => {
           </div>
 
           <div className="flex flex-col gap-2 border-white border md:w-1/2 w-full self-end p-2">
-            <h2 className="text-elh2 font-bold">Story</h2>
-            <p>The year is 2325, Summer, Sweden.</p>
-            <p>
-              The world is in ruins after the advancement of AI and Robotics plunged
-              humanity into a global conflict. Those few humans who survived live
-              among the ruins, and the roaming Mechs. The Mechs are programmed to
-              shoot any potential threat on sight.
-            </p>
-            <p>
-              A local resident of a small village finds theirself fed up with the
-              robotic overlords, after a Mega Mech accidentally flattens their house.
-              The local digs up a nuclear warhead, half buried in their backyard, and
-              straps it in the passenger seat. With their car as their primary weapon
-              they set out on a path to vengeance.
-            </p>
+            <h2 className="md:text-elh2 text-elh4 font-bold">Story</h2>
+            <div className="text-elbodys">
+              <p>The year is 2325, Summer, Sweden.</p>
+              <p>
+                The world is in ruins after the advancement of AI and Robotics plunged
+                humanity into a global conflict. Those few humans who survived live
+                among the ruins, and the roaming Mechs. The Mechs are programmed to
+                shoot any potential threat on sight.
+              </p>
+              <p>
+                A local resident of a small village finds theirself fed up with the
+                robotic overlords, after a Mega Mech accidentally flattens their house.
+                The local digs up a nuclear warhead, half buried in their backyard, and
+                straps it in the passenger seat. With their car as their primary weapon
+                they set out on a path to vengeance.
+              </p>
+            </div>
           </div>
-          <div className="flex flex-col justify-center grow-1">
+          <div className="flex flex-col justify-center grow-1 text-center">
             <NewsBtn hrefPath="/" btnTitle="SIGN UP FOR NEWS" />
           </div>
         </div>
--- src/components/NewsBtn/index.tsx
+++ src/components/NewsBtn/index.tsx
@@ -9,8 +9,8 @@ type Props = NewsBtnProps & HrefProps;
 
 const NewsBtn = ({btnTitle, hrefPath}: Props) => {
     return (
-        <div className="flex justify-center ">
-            <a className="border-white border-1 bg-gradient-to-b from-[#75201D] to-[#FFFFFF] hover:opacity-90 transition-colors p-2 px-4" href={hrefPath} target="_blank">{btnTitle}</a>
+        <div className="flex justify-center">
+            <a className="text-1xl border-white border-1 basis-xs bg-gradient-to-b from-[#75201D] to-[#FFFFFF] hover:opacity-90 transition-colors p-2 px-4" href={hrefPath} target="_blank">{btnTitle}</a>
         </div>
     )
 }
