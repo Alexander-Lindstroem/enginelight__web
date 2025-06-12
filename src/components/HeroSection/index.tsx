fc2109ec1ea3bc3493a65fb47f18ce11a8211974
alelin <alexanderbdjgw@gmail.com>
2025-06-12 15:42:07 +0200

    added max-width to description on hero page

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
             <div className="absolute h-full w-full bg-[url(/hero-section-bg.jpg)] bg-center bg-cover bg-no-repeat blur-sm"></div>
             <div className="flex flex-col gap-5 text-center absolute text-white top-[50%] left-[50%] translate-[-50%]">
                 <h1 className="text-elh1 drop-shadow-(--red-shadow)">EngineLight</h1>
-                <p className="text-elbodym">Discover the Open World of Nosviken, infested with old mechanical war robots, or "Mechs", as the locals call them.</p>
+                <p className="text-elbodym max-w-[400px]">Discover the Open World of Nosviken, infested with old mechanical war robots, or "Mechs", as the locals call them.</p>
                 <div className="flex flex-col md:flex-row gap-4 justify-center">
                     <NewsBtn hrefPath="/" btnTitle="WATCH TRAILER" />
                     <NewsBtn hrefPath="/" btnTitle="ADD TO STEAM" />
