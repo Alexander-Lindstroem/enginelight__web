8247143fbfe806f37f9762991744c7b0ea0d00bd
kavitha0691 <kavitha0691@gmail.com>
2025-06-16 11:40:50 +0200

    changes fonts for free flowing on dynamically resize the screen

--- src/components/TonePage/index.tsx
+++ src/components/TonePage/index.tsx
@@ -16,11 +16,11 @@ export default function TonePage() {
       <div className="absolute inset-0 bg-opacity-0 z-10" />
       <div className="absolute inset-0 z-20 flex items-center justify-end font-elmain px-6 sm:px-10 md:px-20 lg:px-32">
         <div className="w-full max-w-xl text-left text-[var(--color-elwhite)] space-y-5 tracking-wide lg:pl-32 ml-0 lg:ml-[8vw]">
-          <h2 className="text-elh2 sm:text-elh3 md:text-elh1 lg:text-elh1 font-bold max-[180px]:text-elh4">Tone</h2>
-          <p className="text-elbodys sm:text-elbodym md:text-elbodyl lg:text-elbodym leading-8 mb-0 max-[180px]:text-elbodys">
+          <h2 className="text-elh2 md:text-elh1 lg:text-elh1 font-bold max-[180px]:text-elh4">Tone</h2>
+          <p className="text-elbodys sm:text-elbodym md:text-elbodym lg:text-elbodym leading-8 mb-0 max-[180px]:text-elbodys">
             The visuals are dark, gritty and moody but the low-poly stylization keeps it lighthearted.
           </p>
-          <p className="text-elbodys sm:text-elbodym md:text-elbodyl lg:text-elbodym leading-8 max-[180px]:text-elbodys">
+          <p className="text-elbodys sm:text-elbodym md:text-elbodym lg:text-elbodym leading-8 max-[180px]:text-elbodys">
             The dialogue, mechanics and events of the game contrasts this darkness with a lot of goofiness and comedy.
           </p>
         </div>
