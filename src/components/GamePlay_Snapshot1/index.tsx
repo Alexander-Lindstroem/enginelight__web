600ed54b1f3af8fa04b6eaae668e7c6e41e0b665
VaishaliGovindaraj <vaishali.govindaraj28@gmail.com>
2025-06-16 15:37:01 +0200

    removing screen width

--- src/components/GamePlay_Snapshot1/index.tsx
+++ src/components/GamePlay_Snapshot1/index.tsx
@@ -2,7 +2,7 @@ const Gameplay_world = () => {
     return (
 
 
-        <div className="min-h-screen w-screen bg-[url('/GamePlay_snapshot1.jpg')] bg-cover  bg-[rgba(0,0,0,0.2)] bg-blend-multiply bg-no-repeat flex items-start justify-start p-10">
+        <div className="min-h-screen  bg-[url('/GamePlay_snapshot1.jpg')] bg-cover  bg-[rgba(0,0,0,0.2)] bg-blend-multiply bg-no-repeat flex items-start justify-start p-10">
             <div className="bg-black/15 p-6 mt-65 ml-10 rounded-lg z-10 max-w-full xs:max-w-[300px]  md:mt-60 md:ml-15 md:max-w-[500px] lg:max-w-[600px] lg:mt-60 lg:ml-15 xl:max-w-[600px]">
                 <h2 className="font-elmain text-elwhite text-3xl mb-4 font-bold">
                     The world
