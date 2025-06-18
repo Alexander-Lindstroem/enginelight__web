cdb0d3661cf9ded21e4fa88e93cfcc4a4338e85f
VaishaliGovindaraj <vaishali.govindaraj28@gmail.com>
2025-06-18 13:37:55 +0200

    linkedin_link fix

--- src/components/CrewTeam/index.tsx
+++ src/components/CrewTeam/index.tsx
@@ -6,37 +6,49 @@ const CrewTeam = () => {
         <>
             <div className="relative py-12 space-y-8 bg-black font-elmain text-white">
                 <div className="mb-50">
-                <h2 className="text-center p-6 text-xl">Get to know your team</h2>
-                <Image  className="absolute left-[0] tr top-[5%]" src="/el_vec_l.png" alt="vector_left_image" width={400} height={400} />
-                <Image  className="absolute right-[0] top-[10%] " src="/el_vec_r.png" alt="vector_left_image" width={500} height={500} />
-                {TeamMembers.map((category, index) => (
-                    <div key={index} className="relative z-50" >
-                        <h4 className="text-l font-semibold text-center text-white mb-8">
-                            {category.category}
-                        </h4>
+                    <h2 className="text-center p-6 text-xl">Get to know your team</h2>
+                    <Image className="absolute left-[0] tr top-[5%]" src="/el_vec_l.png" alt="vector_left_image" width={400} height={400} />
+                    <Image className="absolute right-[0] top-[10%] " src="/el_vec_r.png" alt="vector_left_image" width={500} height={500} />
+                    {TeamMembers.map((category, index) => (
+                        <div key={index} className="relative z-50" >
+                            <h4 className="text-l font-semibold text-center text-white mb-8">
+                                {category.category}
+                            </h4>
 
-                        <div className="flex flex-wrap justify-center items-center gap-8">
-                            {category.members.map((member, memberIndex) => (
+                            <div className="flex flex-wrap justify-center items-center gap-8">
+                                {category.members.map((member, memberIndex) => (
 
-                                <div
-                                    key={memberIndex} className="w-[200px] flex flex-col items-center hover:scale-105 transition rounded-lg shadow-lg text-xs whitespace-nowrap text-center p-8 mb-2 ">
-                                    <div className="w-20 h-32 rounded-lg overflow-hidden" style={{ boxShadow: '6px 5px 5px rgba(255, 0, 0, 0.3)' }}>
-                                        <Image
-                                            src={member.image}
-                                            alt={member.name} className="w-full h-full object-cover"  width={150} height={150}/>
+                                    <div
+                                        key={memberIndex} className="w-[200px] flex flex-col justify-center items-center hover:scale-105 transition rounded-lg shadow-lg text-xs whitespace-nowrap text-center p-8 mb-2 ">
+                                        <div className="w-20 h-32 rounded-lg overflow-hidden" style={{ boxShadow: '6px 5px 5px rgba(255, 0, 0, 0.3)' }}>
+                                            <Image
+                                                src={member.image}
+                                                alt={member.name} className="w-full h-full object-cover" width={150} height={150} />
+                                        </div>
+                                        <h3 className="font-medium text-m text-white py-4">{member.name}</h3>
+                                        <p className=" text-gray-400 py-5">{member.title}</p>
+                                        {member.linkedin ? (
+                                            <a
+                                                href={`https://www.linkedin.com/in/${member.linkedin}`}
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                            >
+                                                <img
+                                                    src="/linkedin_logo.png"
+                                                    alt="LinkedIn"
+                                                    className="w-6 h-6 bg-blue-600 rounded"
+                                                />
+                                            </a>
+                                        ) : (
+                                            <div className="w-6 h-6 invisible" />
+                                        )}
                                     </div>
-                                    <h3 className="font-medium text-white py-5">{member.name}</h3>
-                                    <p className=" text-gray-400 py-5">{member.title}</p>
-                                    <a href={`https://www.linkedin.com/in/${member.linkedin}`} target="_blank" rel="noopener noreferrer">
-                                        <img src="/linkedin_logo.png" alt="LinkedIn" className="w-6 h-6 bg-blue-600 rounded " />
-                                    </a>
-                                </div>
-                            ))}
+                                ))}
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
                 </div>
-                <Image  className="absolute top-[100%] -translate-y-[100%] right-[50%] translate-x-[50%] z-10 pt-100 " src="/el_vec_b.png" alt="vector_left_image" width={1000} height={1000} />
+                <Image className="absolute top-[100%] -translate-y-[100%] right-[50%] translate-x-[50%] z-10 pt-100 " src="/el_vec_b.png" alt="vector_left_image" width={1000} height={1000} />
             </div>
         </>
     )
--- src/utils/data.ts
+++ src/utils/data.ts
@@ -4,23 +4,29 @@ export const TeamMembers: TeamCategory[] = [
   {
     category: "Management",
     members: [
+       {
+        name: "Kaveh Esmaili",
+        title: "PM WEB",
+        image: "/kaveh.jpg",
+        linkedin: "kaveh-esmaili-4b5b54178/"
+      },
       {
         name: "Joakim Skoglund",
         title: "Producer",
         image: "/joakim_skoglund.jpg",
-        linkedin: ""
+        linkedin: "joakim-skoglund-31352572/"
       },
       {
         name: "Lilja Runarsdottir",
         title: "Project Team Manager",
         image: "/lilja_runarsdottir.jpg",
-        linkedin: ""
+        linkedin: "lilja-heidur-runarsdottir-5670b438/"
       },
       {
         name: "Johnny Englund",
         title: "Release Manager",
         image: "/johnny_englund.jpg",
-        linkedin: ""
+        linkedin: "johnnyenglund/"
       }
     ]
   },
@@ -31,19 +37,19 @@ export const TeamMembers: TeamCategory[] = [
         name: "Jonatan Alexandersson",
         title: "Game Artist",
         image: "/jonatan_alexandersson.jpg",
-        linkedin: ""
+        linkedin: "jonatan-alexandersson-54767433b/"
       },
       {
         name: "Ramesh Pitumpe",
         title: "Game Artist / Art Director",
         image: "/ramesh_pitumpe.jpg",
-        linkedin: ""
+        linkedin: "pixelmorph/"
       },
       {
         name: "Dennis Lindberg",
         title: "Game Artist",
         image: "/dennis_lindberg.jpg",
-        linkedin: ""
+        linkedin: "dennis-lindberg-a1834b286/"
       }
     ]
   },
@@ -54,25 +60,25 @@ export const TeamMembers: TeamCategory[] = [
         name: "Alexander Granskog",
         title: "Game Designer / Product",
         image: "/alexander_granskog.jpg",
-        linkedin: ""
+        linkedin: "grainwood/"
       },
       {
         name: "Aakash Muraleedharan",
         title: "Game Designer",
         image: "/aakash_muraleedharan.jpg",
-        linkedin: ""
+        linkedin: "aakash-muraleedharan-4402441b7/"
       },
       {
         name: "Shekib Jon Sharar",
         title: "Game Designer",
         image: "/shekib_jon_sharar.jpg",
-        linkedin: ""
+        linkedin: "shekib-jon-sharar-817591292/"
       },
       {
         name: "Duarte Susano",
         title: "Game Designer",
         image: "/duarte_susano.jpg",
-        linkedin: ""
+        linkedin: "duarte-susano-gamedeveloper/"
       }
     ]
   },
@@ -89,19 +95,19 @@ export const TeamMembers: TeamCategory[] = [
         name: "Lorik Fejzullahu",
         title: "Programmer",
         image: "/lorik_fejzullahu.jpg",
-        linkedin: ""
+        linkedin: "lorik-fejzullahu-59995b336/"
       },
       {
         name: "Indra Cosimo Bowen-Paoli",
         title: "Game Programmer",
-        image: "/indra_cosimo_bowen_paoli.jpg",
-        linkedin: ""
+        image: "/indra_cosimo_bowen_paoli.png",
+        linkedin: "indra-cosimo-bowen-paoli-a104a71a9/"
       },
       {
         name: "Zhengyang",
         title: "Programmer",
         image: "/Zhengyang.jpg",
-        linkedin: ""
+        linkedin: "zhengyang-lyu-940122/"
       }
     ]
   },
@@ -118,6 +124,29 @@ export const TeamMembers: TeamCategory[] = [
         name: "Arda Boyacloglu Nahmiyas",
         title: "Animation / VFX",
         image: "/arda_boyacloglu_nahmiyas.jpg",
+        linkedin: "arda-boyacioglu-837135203/"
+      }
+    ]
+  },
+   {
+    category: "UI /UX Designer",
+    members: [
+      {
+        name: "Simon Bengtsson",
+        title: "UI/UX Designer",
+        image: "/simon_bengtsson.png",
+        linkedin: ""
+      },
+      {
+        name: "Sebestian Fredriksson",
+        title: "UI/UX Designer",
+        image: "/sebastian_fredriksson.png",
+        linkedin: ""
+      },
+       {
+        name: "Jonah Aronsson",
+        title: "UI/UX Designer",
+        image: "/jonah_aronsson.png",
         linkedin: ""
       }
     ]
@@ -129,13 +158,13 @@ export const TeamMembers: TeamCategory[] = [
         name: "Alexander Lindström",
         title: "Frontend Developer",
         image: "/alexander_lindström.jpg",
-        linkedin: ""
+        linkedin: "alexander-lindström-473692240/"
       },
       {
         name: "Caroline Lindbom",
         title: "Frontend Developer",
         image: "/caroline_lindbom.jpg",
-        linkedin: ""
+        linkedin: "caroline-lindbom-4173b2370/"
       },
       {
         name: "Kavitha Ganesan",
@@ -147,13 +176,13 @@ export const TeamMembers: TeamCategory[] = [
         name: "Vaishali Govindaraj",
         title: "Frontend Developer",
         image: "/vaishali_govindaraj.jpg",
-        linkedin: ""
+        linkedin: "vaishali-govindaraj/"
       },
       {
         name: "Victor Furustubbe",
         title: "Frontend Developer",
         image: "/victor_furustubbe.jpg",
-        linkedin: ""
+        linkedin: "victor-f-405b631a7/"
       }
     ]
   }
