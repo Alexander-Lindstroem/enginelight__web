e4f442db9d1490f12070053bce0e6e2a7ad8e0e4
idacarlin95 <carlin95@hotmail.se>
2025-06-18 15:27:11 +0200

    Changed title

--- src/components/CrewIntro/index.tsx
+++ src/components/CrewIntro/index.tsx
@@ -7,7 +7,7 @@ const CrewIntro = () => {
             <div className="relative z-10 flex flex-col items-center justify-center h-full px-4 sm:px-20">
                 <div className="text-center max-w-2xl sm:max-w-lg md:max-w-xl font-elmain text-elwhite px-34 sm:px-20 md:px-10">
                     <h1 className="text-4xl sm:text-5xl font-semibold mb-5 text-shadow-soft">
-                        Enginelights team
+                        Enginelight team
                     </h1>
                     <p className="text-md sm:text-lg leading-relaxed">
                         Enginelight is a project created by young and talented people from around the world, working together to create an experience of a future nightmare
