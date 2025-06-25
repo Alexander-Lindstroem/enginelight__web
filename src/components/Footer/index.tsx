e441258e2530e37babe62ae35833b46f6589437b
kavitha0691 <kavitha0691@gmail.com>
2025-06-25 14:26:39 +0200

    add font family

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,8 @@
 export default function Footer() {
     return (
-        <footer className=" bg-black bg-opacity-90 text-gray-300 py-8 text-center text-elbodym">
+        <footer className=" border-t-amber-950 border-t-2 bg-black bg-opacity-90 text-gray-300 py-8 px-2 text-center text-elbodym">
             <div className="max-w-3xl mx-auto">
-                <p>&copy; {new Date().getFullYear()} EngineLight. All rights reserved.</p>
+                <p className="font-elmain text-elbodys"> &copy; {new Date().getFullYear()}  EngineLight. All rights reserved.</p>
                 <p className=" font-elmain text-elbodys text-gray-400 mt-1">
                     A game by FutureGames
                 </p>
@@ -10,7 +10,7 @@ export default function Footer() {
                     href="https://futuregames.itch.io/enginelight"
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="text-blue-200 text-elbodys hover:underline mt-2 block break-words"
+                    className="font-elmain text-blue-200 text-elbodys underline hover:text-blue-300 mt-2 block break-words"
                 >
                     futuregames.itch.io/enginelight
                 </a>
